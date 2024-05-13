a895cb4fab74be9fbaa616220e9bf4c534e4c858
Cory <corycalderon95@gmail.com>
2024-05-13 12:58:29 -0400

    completed

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+
+        private double _balance = 0;
+
+        public void Deposite(double cashDeposite)
+        {
+            _balance += cashDeposite;
+        }
+
+        public double CheckBalance()
+        {
+            return _balance;
+        }
+
+        
+    }
+
+
+
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace BankEncapsulation
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics.Contracts;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            var balance = account.CheckBalance();
+            Console.WriteLine($"Your account balance is ${balance}.");
+            Console.WriteLine($"How much would you like to deposite?");
+
+            account.Deposite(50);
+            Console.WriteLine($"You deposited $50.");
+
+            balance = account.CheckBalance();
+            Console.WriteLine($"Your current account balance is now ${balance}.");
+
         }
     }
+    
+
+
+
+
 }
